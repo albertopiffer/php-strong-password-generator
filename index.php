d24a6f5911c4b653f410bff22faf278cb6d883cd
albertopiffer <albertopiffer96@gmail.com>
2023-03-21 18:18:51 +0100

    FEAT milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php
+<!-- <?php
     echo "Hello World";
+?> -->
+
+<form action="index.php" method="GET">
+    <input type="number" name="lunghezza" placeholder="lunghezza">
+    <button type="submit">Invia</button>
+</form>
+
+<?php
+$passwordElements = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+$n = 0;
+$n = $_GET['lunghezza'];
+
+//echo $n . "<br>";
+
+$password = array();
+
+for ($i = 0; $i < $n; $i++) {
+    array_push($password, $passwordElements[rand(0, strlen($passwordElements) - 1)]);
+}
+
+echo implode("", $password);
+
 ?>
